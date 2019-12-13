d6a386f423de0e0bcef88e91bbdad612c1382775
Michael Stevens <codemasterflash@Michaels-MacBook-Pro.local>
2019-12-13 10:19:38 -0600

    added dependencies, folders and files, fixed index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+///////////////////////////////////////
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+create store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, 
+document.getElementById("root"));
